e7eb190f37b5edcc08cc8f8584cdcfe2fd9df664
CAN4red <CAN4red@yandex.ru>
2024-12-17 21:20:57 +0300

    MonthModel updated for SessionOrder

--- app/src/main/java/com/example/alcocalendar/model/MonthModel.kt
+++ app/src/main/java/com/example/alcocalendar/model/MonthModel.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import com.example.alcocalendar.db.entities.DrinkingSession
 import com.example.alcocalendar.db.entities.DrinkingSessionDb
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
 
@@ -24,21 +25,128 @@ data class MonthModel(
     val sessions: List<DrinkingSession>
         get() = _sessions.toList()
 
-    fun getDrinkingSession(date: Int): DrinkingSession {
+    private fun getDrinkingSession(date: Int): DrinkingSessionWrapper {
         return _sessions[date - 1]
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun updateDrinkingSession(session: DrinkingSessionWrapper) {
         _sessions[session.date.dayOfMonth - 1] = session
+        updateSessionOrders(session)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateSessionOrders(session: DrinkingSessionWrapper) {
+        updateSessionOrder(session)
+
+        if (session.date.dayOfMonth > 1) {
+            updateSessionOrder(getDrinkingSession(session.date.dayOfMonth - 1))
+        }
+        if (session.date.dayOfMonth < getLastDayOfMonth(this.year, this.month)) {
+            updateSessionOrder(getDrinkingSession(session.date.dayOfMonth + 1))
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateSessionOrder(session: DrinkingSessionWrapper?) {
+        session?.let {
+            _sessions[it.date.dayOfMonth - 1] = it.copy(
+                sessionOrder = getSessionOrder(it)
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getSessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        if (session.isEmpty) {
+            return SessionOrder.EMPTY_SESSION
+        }
+
+        val dayOfMonth = session.date.dayOfMonth
+
+        return when (dayOfMonth) {
+            1 -> handleFirstDaySessionOrder(session)
+            getLastDayOfMonth(this.year, this.month) -> handleLastDaySessionOrder(session)
+            else -> handleRegularSession(session)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleFirstDaySessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfWeek = session.date.dayOfWeek
+        val dayOfMonth = session.date.dayOfMonth
+        val nextSession = getDrinkingSession(dayOfMonth + 1)
+
+        return when (dayOfWeek == DayOfWeek.SUNDAY || nextSession.isEmpty) {
+            true -> SessionOrder.SINGLE_SESSION
+            false -> SessionOrder.FIRST_IN_ROW
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleLastDaySessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfWeek = session.date.dayOfWeek
+        val dayOfMonth = session.date.dayOfMonth
+        val previousSession = getDrinkingSession(dayOfMonth - 1)
+
+        return when (dayOfWeek == DayOfWeek.MONDAY || previousSession.isEmpty) {
+            true -> SessionOrder.SINGLE_SESSION
+            false -> SessionOrder.LAST_IN_ROW
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleRegularSession(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfWeek = session.date.dayOfWeek
+        return when (dayOfWeek) {
+            DayOfWeek.MONDAY -> handleMondaySessionOrder(session)
+            DayOfWeek.SUNDAY -> handleSundaySessionOrder(session)
+            else -> handleOtherSessionOrder(session)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleMondaySessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfMonth = session.date.dayOfMonth
+        val nextSession = getDrinkingSession(dayOfMonth + 1)
+
+        return when (nextSession.isEmpty) {
+            true -> SessionOrder.SINGLE_SESSION
+            false -> SessionOrder.FIRST_IN_ROW
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleSundaySessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfMonth = session.date.dayOfMonth
+        val previousSession = getDrinkingSession(dayOfMonth - 1)
+
+        return when (previousSession.isEmpty) {
+            true -> SessionOrder.SINGLE_SESSION
+            false -> SessionOrder.LAST_IN_ROW
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleOtherSessionOrder(session: DrinkingSessionWrapper): SessionOrder {
+        val dayOfMonth = session.date.dayOfMonth
+        val nextSession = getDrinkingSession(dayOfMonth + 1)
+        val previousSession = getDrinkingSession(dayOfMonth - 1)
+
+        return when {
+            !previousSession.isEmpty && !nextSession.isEmpty -> SessionOrder.MIDDLE_IN_ROW
+            !previousSession.isEmpty && nextSession.isEmpty -> SessionOrder.LAST_IN_ROW
+            previousSession.isEmpty && !nextSession.isEmpty -> SessionOrder.FIRST_IN_ROW
+            else -> SessionOrder.SINGLE_SESSION
+        }
     }
 }
 
 
 @SuppressLint("NewApi")
-private fun getLastDayOfMonth(year: Int, month: Month): LocalDate {
+private fun getLastDayOfMonth(year: Int, month: Month): Int {
     val isLeapYear = year.isLeapYear()
-    return LocalDate.of(year, month, month.length(isLeapYear))
+    return LocalDate.of(year, month, month.length(isLeapYear)).dayOfMonth
 }
 
 
@@ -49,7 +157,7 @@ private fun Int.isLeapYear(): Boolean {
 
 @SuppressLint("NewApi")
 private fun generateEmptySessions(year: Int, month: Month): List<DrinkingSessionWrapper> {
-    val lastDayOfMonth = getLastDayOfMonth(year, month).dayOfMonth
+    val lastDayOfMonth = getLastDayOfMonth(year, month)
     return (1..lastDayOfMonth).map { day ->
         DrinkingSessionWrapper(
             drinkingSession = DrinkingSessionDb(LocalDate.of(year, month, day))
--- app/src/main/java/com/example/alcocalendar/model/SessionOrder.kt
+++ app/src/main/java/com/example/alcocalendar/model/SessionOrder.kt
@@ -1,6 +1,7 @@
 package com.example.alcocalendar.model
 
 enum class SessionOrder {
+    SINGLE_SESSION,
     FIRST_IN_ROW,
     MIDDLE_IN_ROW,
     LAST_IN_ROW,
