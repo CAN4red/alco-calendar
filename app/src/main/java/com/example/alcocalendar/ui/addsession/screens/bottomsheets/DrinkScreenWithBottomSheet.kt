893ceff1de81353cf91d7e80cf88e1d212b501a8
CAN4red <CAN4red@yandex.ru>
2024-12-26 20:22:25 +0300

    Visual bug with Bottom Sheet fixed

--- app/src/main/java/com/example/alcocalendar/ui/addsession/screens/bottomsheets/DrinkScreenWithBottomSheet.kt
+++ app/src/main/java/com/example/alcocalendar/ui/addsession/screens/bottomsheets/DrinkScreenWithBottomSheet.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,6 +38,12 @@ fun DrinkScreenWithBottomSheet(
     val textFieldViewModel: TextFieldViewModel = viewModel()
     val textFieldState = textFieldViewModel.textFieldState.collectAsState()
     val textFieldStateDouble = textFieldViewModel.textFieldStateAsDouble.collectAsState()
+
+    LaunchedEffect(sheetState.isVisible) {
+        if (sheetState.isVisible) {
+            sheetState.expand()
+        }
+    }
 
     content { newIntake ->
         currentIntakeState = newIntake
