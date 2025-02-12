0746ee4613cff33612b4b44e956fd3d71d31984e
can4red <CAN4red@yandex.ru>
2025-02-12 21:38:06 +0300

    DrinkingSessionDatabase updated

--- app/src/main/java/com/example/alcocalendar/core/data/local/DrinkingSessionDatabase.kt
+++ app/src/main/java/com/example/alcocalendar/core/data/local/DrinkingSessionDatabase.kt
@@ -7,21 +7,30 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.alcocalendar.core.data.local.converters.DrinkTypeConverter
 import com.example.alcocalendar.core.data.local.converters.LocalDateConverter
+import com.example.alcocalendar.core.data.local.converters.YearMonthTypeConverter
+import com.example.alcocalendar.core.data.local.converters.YearTypeConverter
 import com.example.alcocalendar.core.data.local.dao.DrinkingSessionDao
-import com.example.alcocalendar.core.data.local.entities.DrinkingSession
+import com.example.alcocalendar.core.data.local.entities.DrinkIntakeEntity
+import com.example.alcocalendar.core.data.local.entities.DrinkingSessionEntity
+import com.example.alcocalendar.features.calendar.data.local.dao.CalendarDao
 import com.example.alcocalendar.features.drink_intake.data.local.dao.DrinkIntakeDao
-import com.example.alcocalendar.core.data.local.entities.DrinkIntake
 
 @Database(
-    entities = [DrinkingSession::class, DrinkIntake::class],
-    version = 2,
+    entities = [DrinkingSessionEntity::class, DrinkIntakeEntity::class],
+    version = 3,
     exportSchema = false,
 )
-@TypeConverters(LocalDateConverter::class, DrinkTypeConverter::class)
+@TypeConverters(
+    LocalDateConverter::class,
+    DrinkTypeConverter::class,
+    YearMonthTypeConverter::class,
+    YearTypeConverter::class
+)
 abstract class DrinkingSessionDatabase : RoomDatabase() {
 
     abstract val drinkingSessionDao: DrinkingSessionDao
-    abstract val drinkIntakeDao : DrinkIntakeDao
+    abstract val drinkIntakeDao: DrinkIntakeDao
+    abstract val calendarDao: CalendarDao
 
     companion object {
         @Volatile
@@ -29,7 +38,11 @@ abstract class DrinkingSessionDatabase : RoomDatabase() {
 
         fun getInstance(context: Context): DrinkingSessionDatabase {
             return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(context, DrinkingSessionDatabase::class.java, "drinking_session_database")
+                Room.databaseBuilder(
+                    context,
+                    DrinkingSessionDatabase::class.java,
+                    "drinking_session_database"
+                )
                     .fallbackToDestructiveMigration()
                     .build()
                     .also { INSTANCE = it }
