6a62802ec1d2a5e744181f9102b22b3bdc78781b
can4red <CAN4red@yandex.ru>
2025-02-12 15:16:08 +0300

    DrinkIntake updated

--- app/src/main/java/com/example/alcocalendar/core/data/local/entities/DrinkIntake.kt
+++ app/src/main/java/com/example/alcocalendar/core/data/local/entities/DrinkIntake.kt
@@ -19,10 +19,20 @@ import java.time.LocalDate
     ]
 )
 data class DrinkIntake(
+    @PrimaryKey(autoGenerate = true)
+    val drinkIntakeId: Int = 0,
     val date: LocalDate,
     val drinkType: DrinkType,
     val liters: Double = 0.0,
     val alcoStrength: Double = drinkType.defaultAlcoStrength,
-    @PrimaryKey(autoGenerate = true)
-    val drinkIntakeId: Int = 0,
-)
+) {
+    val isEmpty: Boolean
+        get() = (liters == 0.0)
+
+    val alcoUnits: Double
+        get() = liters * alcoStrength * ETHANOL_MASS
+
+    companion object {
+        private const val ETHANOL_MASS = 0.789
+    }
+}
