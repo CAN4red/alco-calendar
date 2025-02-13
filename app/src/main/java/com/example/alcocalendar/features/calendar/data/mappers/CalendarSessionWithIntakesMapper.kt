d2bf7f075f618e6a499b33876271178718d6155f
can4red <CAN4red@yandex.ru>
2025-02-13 14:40:28 +0300

    Mapper for domain model of calendar session added

---
+++ app/src/main/java/com/example/alcocalendar/features/calendar/data/mappers/CalendarSessionWithIntakesMapper.kt
@@ -0,0 +1,15 @@
+package com.example.alcocalendar.features.calendar.data.mappers
+
+import com.example.alcocalendar.core.data.local.relations.DrinkingSessionWithDrinkIntakes
+import com.example.alcocalendar.core.data.mappers.DrinkIntakeMapper
+import com.example.alcocalendar.features.calendar.domain.model.CalendarSessionWithIntakes
+
+object CalendarSessionWithIntakesMapper {
+
+    fun toDomain(sessionWithIntakes: DrinkingSessionWithDrinkIntakes): CalendarSessionWithIntakes {
+        return CalendarSessionWithIntakes(
+            date = sessionWithIntakes.drinkingSession.date,
+            intakes = sessionWithIntakes.drinkIntakes.map { DrinkIntakeMapper.toDomain(it) }
+        )
+    }
+}
