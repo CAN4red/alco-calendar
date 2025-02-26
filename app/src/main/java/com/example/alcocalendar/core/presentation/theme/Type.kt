8751cb64c1ef0a537b545f542088c0cd0ba8c8c3
can4red <CAN4red@yandex.ru>
2025-02-26 14:07:13 +0300

    feat: add typography style

--- app/src/main/java/com/example/alcocalendar/core/presentation/theme/Type.kt
+++ app/src/main/java/com/example/alcocalendar/core/presentation/theme/Type.kt
@@ -2,33 +2,58 @@ package com.example.alcocalendar.core.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.alcocalendar.R
 
-// Set of Material typography styles to start with
+val ReadexPro = FontFamily(
+    Font(R.font.readex_pro_light, FontWeight.Light),
+    Font(R.font.readex_pro, FontWeight.Normal),
+    Font(R.font.readex_pro_medium, FontWeight.Medium),
+    Font(R.font.readex_pro_semibold, FontWeight.SemiBold),
+    Font(R.font.readex_pro_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(
+        fontFamily = ReadexPro,
         fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = ReadexPro,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = ReadexPro,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.15.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    bodyMedium = TextStyle(
+        fontFamily = ReadexPro,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = ReadexPro,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.5.sp,
+    ),
 )
