fd4a3de197cb2f9ae753f0c55fe611f020193b0c
can4red <CAN4red@yandex.ru>
2025-03-06 18:50:09 +0300

    feat: add read only field for notes

---
+++ app/src/main/java/com/example/alcocalendar/features/session_manage/notes/presentation/components/NotesField.kt
@@ -0,0 +1,46 @@
+package com.example.alcocalendar.features.session_manage.notes.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NotesField(
+    onClick: () -> Unit,
+    content: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .clickable { onClick() }
+    ) {
+        Text(text = content)
+    }
+}
+
+@Preview
+@Composable
+private fun NotesFieldPreview() {
+    NotesField(
+        onClick = {},
+        content = "Lorem Ipsum is simply dummy text of" +
+                "the printing and typesetting industry." +
+                "Lorem Ipsum has been the industry's" +
+                "standard dummy text ever since the" +
+                "1500s, when an unknown printer took a" +
+                "galley of type and scrambled it to make a" +
+                "type specimen book. \n" +
+                "\n" +
+                "It has survived not only five centuries," +
+                "but also the leap into electronic" +
+                "typesetting, remaining essentially..."
+    )
+}
