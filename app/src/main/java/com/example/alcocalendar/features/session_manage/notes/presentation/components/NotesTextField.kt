2021a75e2f2380a7ad4449b39289d756a00992e8
can4red <CAN4red@yandex.ru>
2025-03-12 14:54:42 +0300

    feat: add cursor focusing

--- app/src/main/java/com/example/alcocalendar/features/session_manage/notes/presentation/components/NotesTextField.kt
+++ app/src/main/java/com/example/alcocalendar/features/session_manage/notes/presentation/components/NotesTextField.kt
@@ -2,12 +2,16 @@ package com.example.alcocalendar.features.session_manage.notes.presentation.comp
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -17,9 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.alcocalendar.features.session_manage.notes.domain.model.Note
@@ -50,6 +58,15 @@ fun NotesTextField(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.surface)
+            .imePadding()
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = {
+                    focusManager.moveFocus(FocusDirection.Up)
+                    textFieldValue = textFieldValue.getFocusedOnLastLine()
+                }
+            )
     ) {
         BasicTextField(
             value = textFieldValue,
@@ -58,6 +75,11 @@ fun NotesTextField(
                 onEvent(NotesEvent.SetNoteContent(newValue.text))
             },
             textStyle = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onSurface),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.None,
+            ),
+            singleLine = false,
             modifier = Modifier.fillMaxWidth()
         )
     }
@@ -67,6 +89,10 @@ fun NotesTextField(
 private fun keyboardAsState(): State<Boolean> {
     val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
     return rememberUpdatedState(isImeVisible)
+}
+
+private fun TextFieldValue.getFocusedOnLastLine(): TextFieldValue {
+    return this.copy(selection = TextRange(this.text.length))
 }
 
 @Preview
