25ff04b5c16c8640c8aba05e15567b98d4f12697
can4red <CAN4red@yandex.ru>
2025-03-12 19:08:10 +0300

    feat: add default note and min height to the notes field

--- app/src/main/java/com/example/alcocalendar/features/session_manage/notes/presentation/components/NotesField.kt
+++ app/src/main/java/com/example/alcocalendar/features/session_manage/notes/presentation/components/NotesField.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -13,8 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.alcocalendar.R
 
 @Composable
 fun NotesField(
@@ -25,6 +30,7 @@ fun NotesField(
     Column(
         modifier = modifier
             .fillMaxWidth()
+            .heightIn(min = getMinNotesSize())
             .clip(RoundedCornerShape(topStart = 18.dp, topEnd = 18.dp))
             .background(MaterialTheme.colorScheme.surface)
             .clickable(
@@ -35,13 +41,18 @@ fun NotesField(
             .padding(12.dp)
     ) {
         NotesTitle()
-
         Text(
-            text = content,
+            text = content.ifBlank { stringResource(R.string.default_note) },
             style = MaterialTheme.typography.bodySmall,
             color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
         )
     }
+}
+
+@Composable
+fun getMinNotesSize(): Dp {
+    val screenHeight = LocalConfiguration.current.screenHeightDp
+    return (screenHeight * 0.24).dp
 }
 
 @Preview
