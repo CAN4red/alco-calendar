da38f7928e4adc8c60155473170b11ef66e4b3d8
can4red <CAN4red@yandex.ru>
2025-03-17 18:35:28 +0300

    feat: add media item dao

---
+++ app/src/main/java/com/example/alcocalendar/features/session_manage/media/data/local/dao/MediaDao.kt
@@ -0,0 +1,24 @@
+package com.example.alcocalendar.features.session_manage.media.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.example.alcocalendar.features.session_manage.media.data.local.entity.MediaItemEntity
+import com.example.alcocalendar.features.session_manage.media.data.local.entity.relations.DrinkingSessionWithMediaItems
+import java.time.LocalDate
+
+@Dao
+interface MediaDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMediaItem(mediaItem: MediaItemEntity)
+
+    @Query("DELETE FROM media_item WHERE name = :fileName")
+    suspend fun deleteMediaItemByName(fileName: String)
+
+    @Transaction
+    @Query("SELECT * FROM drinking_session WHERE date = :date")
+    suspend fun getDrinkingSessionWithMediaItems(date: LocalDate): DrinkingSessionWithMediaItems
+}
