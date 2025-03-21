b4a5bb4e3dbf4ea5c6238486bc8ee0b3447d6b5e
can4red <CAN4red@yandex.ru>
2025-03-21 20:56:54 +0300

    refactor: replace Image by AsyncImage

--- app/src/main/java/com/example/alcocalendar/features/session_manage/media/presentation/components/ImageExpandedItem.kt
+++ app/src/main/java/com/example/alcocalendar/features/session_manage/media/presentation/components/ImageExpandedItem.kt
@@ -1,16 +1,13 @@
 package com.example.alcocalendar.features.session_manage.media.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.core.net.toUri
+import coil3.compose.AsyncImage
 import com.example.alcocalendar.R
-import com.example.alcocalendar.features.session_manage.common.mappers.MediaMapper.toBitmap
 import com.example.alcocalendar.features.session_manage.media.domain.model.MediaItem
 
 @Composable
@@ -18,15 +15,10 @@ fun ImageExpandedItem(
     mediaItem: MediaItem,
     modifier: Modifier = Modifier
 ) {
-    val bitmap = remember(mediaItem.content) {
-        mediaItem.content.toBitmap()
-    }
-
     Box(modifier = modifier) {
-        Image(
-            bitmap = bitmap.asImageBitmap(),
+        AsyncImage(
+            model = mediaItem.path?.toUri() ?: "", // TODO: add default uri
             contentDescription = stringResource(R.string.image_media_item),
-            contentScale = ContentScale.Fit,
             modifier = Modifier.fillMaxSize()
         )
     }
